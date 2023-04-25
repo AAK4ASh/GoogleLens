f6add78ac0542ba50d1f699b1a3d8f0e62544b70
Aakash <mr.aakash726@gmail.com>
2023-04-25 22:45:15 +0530

    bar code

--- app/src/main/java/com/main/googlelens/MainActivity.kt
+++ app/src/main/java/com/main/googlelens/MainActivity.kt
@@ -1,178 +1,24 @@
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.ContentValues
-import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.widget.Toast
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.video.*
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.common.InputImage
+import android.os.Bundle
+import com.main.googlelens.BarCodeFragment
+import com.main.googlelens.FaceDetectionFragment
 import com.main.googlelens.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-typealias LumaListener = (luma: Double) -> Unit
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
-    private var imageCapture: ImageCapture? = null
-
-
-    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Request camera permissions
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
-
-        // Set up the listeners for take photo and video capture buttons
-        binding.imageCaptureButton.setOnClickListener { takePhoto() }
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
-    }
-
-    private fun takePhoto() {
-        val imageCapture=imageCapture?:return
-        val name =SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues= ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME,name)
-            put(MediaStore.MediaColumns.MIME_TYPE,"images/jpeg")
-            if (Build.VERSION.SDK_INT>Build.VERSION_CODES.P){
-                put(MediaStore.Images.Media.RELATIVE_PATH,"Pictures/CameraX-Image")
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        viewBinding.apply {
+            imageCaptureButton.setOnClickListener{
+                startActivity(Intent(this@MainActivity, BarCodeFragment::class.java))
+            }
+            videoCaptureButton.setOnClickListener {
+                startActivity(Intent(this@MainActivity, FaceDetectionFragment::class.java))
             }
         }
-        // Create output options object which contains file + metadata
-
-        val outputOptions= ImageCapture.OutputFileOptions.Builder(
-            contentResolver,MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues
-        ).build()
-        imageCapture.takePicture(
-            outputOptions,ContextCompat.getMainExecutor(this),
-            object :ImageCapture.OnImageSavedCallback{
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    val msg="Photo captured Succeeded:${outputFileResults.savedUri}"
-                    Toast.makeText(baseContext,msg,Toast.LENGTH_SHORT).show()
-                    Log.d(TAG,msg)
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    Log.d(TAG,"Photo Capture Failed",exception)
-                }
-            }
-        )
     }
-
-
-
-    private fun startCamera() {
-        val cameraFutureProvider= ProcessCameraProvider.getInstance(this)
-        cameraFutureProvider.addListener({
-            val cameraProvider:ProcessCameraProvider=  cameraFutureProvider.get()
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-                }
-            imageCapture= ImageCapture.Builder().build()
-            val imageAnalyzer =ImageAnalysis.Builder()
-                .build()
-                .also{
-                    it.setAnalyzer(cameraExecutor,LuminosityAnalyzer{luma ->
-                        Log.d(TAG,"Average luminosity: $luma")
-                    })
-                }
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageCapture,imageAnalyzer)
-            }
-            catch (exc:Exception){
-                Log.d(TAG,"Use case binding failed",exc)
-            }
-
-        },ContextCompat.getMainExecutor(this))
-    }
-
-     private class LuminosityAnalyzer(private val listener:LumaListener):ImageAnalysis.Analyzer
-     {
-         val scanner = BarcodeScanning.getClient()
-         @SuppressLint("UnsafeOptInUsageError")
-         override fun analyze(image: ImageProxy) {
-image.image?.let{
-    val inputImage = InputImage.fromMediaImage(it,
-    image.imageInfo.rotationDegrees)
-    scanner.process(inputImage).addOnSuccessListener {
-        it.forEach{
-            Log.d(TAG,"BARCODE: ${it.rawValue}")
-        }
-    }.addOnFailureListener{
-        it.printStackTrace()
-    }.addOnCompleteListener {
-        image.close()
-    }
-
 }
-         }
-
-     }
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
-    }
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
-
-    companion object {
-        private const val TAG = "CameraXApp"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf (
-                Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
-            }.toTypedArray()
-    }
-
-}
-
